79fd8618a7261ed48b3079bce574bf223934e4ba
Michael Gutierrez <findmagpie@gmail.com>
2024-08-01 19:37:02 -0700

    the basic interface exists

---
+++ src/lib/components/LoadingScreen.svelte
@@ -0,0 +1,26 @@
+<script>
+    // Define the LoadingScreen component
+    export let isLoading = true;
+</script>
+
+<style>
+    .loading-screen {
+        position: fixed;
+        top: 0;
+        left: 0;
+        width: 100%;
+        height: 100%;
+        background-color: #000;
+        color: #fff;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        font-size: 2rem;
+    }
+</style>
+
+{#if isLoading}
+    <div class="loading-screen">
+        <h1>Loading...</h1>
+    </div>
+{/if}
--- src/lib/components/Scene.svelte
+++ src/lib/components/Scene.svelte
@@ -1,81 +1,49 @@
 <script>
-  import { T } from '@threlte/core'
-  import { ContactShadows, Float, Grid, OrbitControls } from '@threlte/extras'
+  import { Grid, Suspense } from '@threlte/extras'
+  import Ssj from './models/ssj.svelte'
+  import { T, useThrelte } from '@threlte/core'
+  import { HemisphereLight, Vector3 } from 'three'
+  import CameraControls from './CameraControls.svelte'
+  import { cameraControls } from './stores.js'
+  import LoadingScreen from './LoadingScreen.svelte'
+
+  let cam
+
+  const EPS = 1e-5
+
+  const { renderer } = useThrelte()
+
+  // renderer.toneMappingExposure = 1
+
 </script>
 
-<T.PerspectiveCamera
-  makeDefault
-  position={[-10, 10, 10]}
-  fov={15}
->
-  <OrbitControls
-    autoRotate
-    enableZoom={false}
-    enableDamping
-    autoRotateSpeed={0.5}
-    target.y={1.5}
-  />
-</T.PerspectiveCamera>
-
-<T.DirectionalLight
-  intensity={0.8}
-  position.x={5}
-  position.y={10}
-/>
-<T.AmbientLight intensity={0.2} />
-
-<Grid
-  position.y={-0.001}
-  cellColor="#ffffff"
-  sectionColor="#ffffff"
-  sectionThickness={0}
-  fadeDistance={25}
-  cellSize={2}
+<T.AmbientLight intensity={6} />
+<T.HemisphereLight
+  intensity={6}
 />
 
-<ContactShadows
-  scale={10}
-  blur={2}
-  far={2.5}
-  opacity={0.5}
-/>
-
-<Float
-  floatIntensity={1}
-  floatingRange={[0, 1]}
->
-  <T.Mesh
-    position.y={1.2}
-    position.z={-0.75}
+<T.Group>
+  <T.PerspectiveCamera
+    makeDefault
+    fov={100}
+    position={[0, .8, 0]}
+    bind:ref={cam}
   >
-    <T.BoxGeometry />
-    <T.MeshStandardMaterial color="#0059BA" />
-  </T.Mesh>
-</Float>
+    <CameraControls
+      on:create={({ ref }) => {
+        $cameraControls = ref
+        $cameraControls.azimuthRotateSpeed = - 0.3 // negative value to invert rotation direction
+        $cameraControls.polarRotateSpeed   = - 0.3 // negative value to invert rotation direction
+        $cameraControls.truckSpeed = 10
+        $cameraControls.dollySpeed = 0
+        $cameraControls.setOrbitPoint(0, .8, EPS)
+        $cameraControls.lookInDirectionOf(0, 1, 5)
+      }}
+    />
+  </T.PerspectiveCamera>
+</T.Group>
 
-<Float
-  floatIntensity={1}
-  floatingRange={[0, 1]}
->
-  <T.Mesh
-    position={[1.2, 1.5, 0.75]}
-    rotation.x={5}
-    rotation.y={71}
-  >
-    <T.TorusKnotGeometry args={[0.5, 0.15, 100, 12, 2, 3]} />
-    <T.MeshStandardMaterial color="#F85122" />
-  </T.Mesh>
-</Float>
-
-<Float
-  floatIntensity={1}
-  floatingRange={[0, 1]}
->
-  <T.Mesh
-    position={[-1.4, 1.5, 0.75]}
-    rotation={[-5, 128, 10]}
-  >
-    <T.IcosahedronGeometry />
-    <T.MeshStandardMaterial color="#F8EBCE" />
-  </T.Mesh>
-</Float>
+<Suspense>
+  <LoadingScreen slot="fallback" />
+  <Ssj />
+</Suspense>
